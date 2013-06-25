aab58cf59dff7f9d006e04c6233a6a0857a548c3
learntechsp <learntechsp@gmail.com>
2013-06-25 23:27:34 +0530

    Test git commit with helloworld

---
+++ helloworld/src/main/java/org/learn/tech/helloworld/App.java
@@ -0,0 +1,13 @@
+package org.learn.tech.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
